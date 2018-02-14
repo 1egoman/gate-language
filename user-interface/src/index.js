bc0dc6a2027b5a9b1f1619292a2520a7fefffc7e
Ryan Gaus <rsg1egoman@gmail.com>
2018-02-14 07:45:26 -0500

    code: add a preview mode to the client which connects over websockets
    The client does this:
    1. Connects over websockets to the locally running preview server.
    2. Waits for a push from the server.
    3. When it receives a push, it renders the gates / wires / contexts contained within.
    4. Running is still accomplished by making a http request to the server.

--- user-interface/src/index.js
+++ user-interface/src/index.js
@@ -7,6 +7,7 @@ import 'codemirror/addon/mode/simple';
 import './index.css';
 
 import renderViewport from './render';
+import queryString from 'query-string';
 
 // import deepDiff from 'deep-diff';
 import debounce from 'lodash.debounce';
@@ -14,6 +15,45 @@ import debounce from 'lodash.debounce';
 import registerServiceWorker from './registerServiceWorker';
 registerServiceWorker();
 
+const query = queryString.parse(window.location.search);
+
+const server = query.server || 'http://localhost:8080';
+const websocketsServer = (query.server || 'http://localhost:8080').replace('http', 'ws');
+
+// When in preview mode, don't render an editor. Instead, connect over websockets to a server
+// running on the local system and whenever a new ast update is pushed, update what is shown in the
+// visualization.
+const previewMode = Boolean(query.preview);
+if (previewMode) {
+  document.getElementById('resize-bar').style.display = 'none';
+  document.getElementById('editor-parent').style.display = 'none';
+
+  // Create websocket server connection
+  function connect() {
+    const ws = new WebSocket(`${websocketsServer}/v1/websocket`);
+    ws.onmessage = function(event) {
+      const payload = JSON.parse(event.data);
+      if (payload.Gates) {
+        data = payload;
+        error = null;
+
+        // Position gates on the screen
+        data = positionGates(data);
+      } else {
+        error = payload.Error;
+      }
+
+      // Rerender using the data received.
+      renderFrame(data.Gates.map(i => i.Id));
+    }
+
+    // On close, wait three seconds and try to connect again.
+    ws.onclose = function(event) {
+      setTimeout(connect, 3000);
+    }
+  }
+  connect();
+}
 
 
 function createEditor(element) {
@@ -45,20 +85,7 @@ function createEditor(element) {
   // Create editor
   const editor = CodeMirror(element, {
     lineNumbers: true,
-    value: `
-import latch
-
-block main() {
-  let clock = momentary()
-  let input = toggle()
-  let a0 a1 a2 a3 output = shiftregister4(clock input)
-  led(a0)
-  led(a1)
-  led(a2)
-  led(a3)
-}
-main()
-    `,
+    value: ``,
     mode: 'bitlang',
     theme: 'monokai',
   });
@@ -66,11 +93,166 @@ main()
 
   return editor;
 }
-const editor = createEditor(document.getElementById('editor-parent'));
+
+// Given the response from a compilation, position all the gates on the screen.
+function positionGates(data) {
+  function getContext(id) {
+    return data.Contexts.find(i => i.Id === id);
+  }
+
+  // Figure out all blocks that this gate network is made up of.
+  const contextsSortedFromShallowestToDeepest = data.Contexts.sort((a, b) => a.Depth - b.Depth);
+
+  // Position each block
+  let rootContextX = 0, rootContextY = 0;
+  contextsSortedFromShallowestToDeepest.forEach(context => {
+    // Get parent and child contexts to the currently active context.
+    const parent = getContext(context.Parent) || {};
+    const childIndex = parent.Children ? parent.Children.findIndex(i => i === context.Id) : 0;
+    const children = context.Children.map(getContext);
+
+    context.x = (parent.x || rootContextX) + (context.Depth * (500 / 4)) + ((childIndex * 2) * 500)
+    context.y = (parent.y || rootContextY) + (context.Depth * (500 / 4))
+    context.width = 500 + (children.length * 500)
+    context.height = 500 + (children.length * 500)
+
+    // If in the root context, increment the position for hte next root block.
+    if (!parent.x) {
+      rootContextX += context.width;
+      rootContextY += 0 //context.height;
+    }
+  });
+
+  let spacingByContext = {};
+  data.Gates.forEach(gate => {
+    // Calculate the width of this gate.
+    let gateWidth = 30;
+    if (gate.Type === 'BLOCK_INPUT' || gate.Type === 'BLOCK_OUTPUT') {
+      gateWidth = 20;
+    }
+    if (gate.Type === 'BUILTIN_FUNCTION' && gate.Label === 'tflipflop') {
+      gateWidth = 80;
+    }
+    gate.width = gateWidth;
+
+    const context = getContext(gate.CallingContext);
+    if (context) {
+      if (gate.Type === 'BLOCK_INPUT' || gate.Type === 'BLOCK_OUTPUT') {
+        // All inputs are positioned on the left border, and all outputs on the right
+        spacingByContext[context.Id] = spacingByContext[context.Id] || 0
+        gate.xPosition = context.x + (gate.Type === 'BLOCK_OUTPUT' ? context.width : 0)
+        gate.yPosition = context.y + spacingByContext[context.Id]
+        context.gateCount += 1
+
+        spacingByContext[context.Id] += gateWidth
+      } else {
+        // All the rest of the gates in a line below the inputs and outputs
+        spacingByContext[context.Id] = spacingByContext[context.Id] || 0
+        gate.xPosition = context.x + spacingByContext[context.Id]
+        gate.yPosition = context.y + 100
+        context.gateCount += 1
+        spacingByContext[context.Id] += gateWidth
+      }
+    } else {
+      // All the rest of the gates in a line below the inputs and outputs
+      spacingByContext[null] = spacingByContext[null] || 0
+      gate.xPosition = spacingByContext[null];
+      gate.yPosition = 0
+      spacingByContext[null] += gateWidth
+    }
+  });
+
+  // Move inputs and outputs closer to the gates that they conenct to
+  data.Gates.forEach(gate => {
+    if (!(gate.Type === 'BLOCK_INPUT' || gate.Type === 'BLOCK_OUTPUT')) {
+      return;
+    }
+
+    const context = getContext(gate.CallingContext);
+
+    const gatesInContext = data.Gates
+      .filter(i => i.Type === gate.Type && i.CallingContext.toString() === context.Id.toString());
+
+    const positionOfGateInBlock = gatesInContext.findIndex(i => i.Id === gate.Id);
+
+    if (gate.Type === 'BLOCK_OUTPUT') {
+      gate.xPosition -= 40;
+    }
+    gate.yPosition = context.y + positionOfGateInBlock * 30;
+  });
+
+  // Move gates closer to their inputs and outputs
+  data.Gates.forEach(gate => {
+    if (gate.Inputs.length === 0 || gate.Type === 'BLOCK_INPUT' || gate.Type === 'BLOCK_OUTPUT') {
+      return;
+    }
+
+    const gateConnectedToInput = data.Gates.find(g => {
+      return g.Outputs.map(k => k.Id).indexOf(gate.Inputs[0].Id) !== -1;
+    });
+
+    if (gateConnectedToInput) {
+      const wireLength = Math.sqrt(
+        Math.pow(gateConnectedToInput.xPosition - gate.xPosition, 2),
+        Math.pow(gateConnectedToInput.yPosition - gate.yPosition, 2),
+      );
+
+      if (wireLength > 100) {
+        gate.xPosition = gateConnectedToInput.xPosition + 50;
+        gate.yPosition = gateConnectedToInput.yPosition + 100;
+      }
+    }
+  });
+
+  // Final positioning step - make sure that gates don't intersect
+  data.Gates.forEach((gate, index) => {
+    if (gate.Type === 'BLOCK_INPUT' || gate.Type === 'BLOCK_OUTPUT') {
+      return;
+    }
+
+    data.Gates.slice(index + 1)
+      .filter(i => !(i.Type === 'BLOCK_INPUT' || i.Type === 'BLOCK_OUTPUT'))
+      .filter(i =>  // Find other gates that intersect with this gate.
+        i.xPosition >= gate.xPosition && i.xPosition <= gate.xPosition + 30 &&
+        i.yPosition >= gate.yPosition && i.yPosition <= gate.yPosition + 30
+      ).forEach((i, ct) => {
+        i.xPosition += ((ct + 1) * 40) 
+        // T flipflops are wider than normal gates, so add a bit of padding.
+        if (gate.Type === 'BUILTIN_FUNCTION' && gate.Label === 'tflipflop') {
+          i.xPosition += 60
+        }
+        i.yPosition += 10
+      });
+  });
+
+  // Rotate gates to try to ensure that they are optimally placed
+  data.Gates.forEach(gate => {
+    if (gate.Outputs.length === 0 || gate.Type === 'BUILTIN_FUNCTION' || gate.Type === 'BLOCK_INPUT' || gate.Type === 'BLOCK_OUTPUT') {
+      return;
+    }
+
+    const gateConnectedToInput = data.Gates.find(g => {
+      return g.Inputs.map(k => k.Id).indexOf(gate.Outputs[0].Id) !== -1;
+    });
+
+    if (gateConnectedToInput && gateConnectedToInput.yPosition > gate.yPosition + 50) {
+      gate.rotate = 180;
+    } else {
+      gate.rotate = 0;
+    }
+  });
+
+  return data;
+}
+
+// If in normal editing mode, then render an editor
+const editor = !previewMode ?
+  createEditor(document.getElementById('editor-parent')) :
+  null;
 
 
 const compile = debounce(function compile(source) {
-  return fetch('http://localhost:8080/v1/compile', {
+  return fetch(`${server}/v1/compile`, {
     method: 'POST',
     body: source,
     headers: {
@@ -96,150 +278,7 @@ const compile = debounce(function compile(source) {
     data.Wires = data.Wires || []
     data.Contexts = data.Contexts || []
 
-    function getContext(id) {
-      return data.Contexts.find(i => i.Id === id);
-    }
-
-    // Figure out all blocks that this gate network is made up of.
-    const contextsSortedFromShallowestToDeepest = data.Contexts.sort((a, b) => a.Depth - b.Depth);
-
-    // Position each block
-    let rootContextX = 0, rootContextY = 0;
-    contextsSortedFromShallowestToDeepest.forEach(context => {
-      // Get parent and child contexts to the currently active context.
-      const parent = getContext(context.Parent) || {};
-      const childIndex = parent.Children ? parent.Children.findIndex(i => i === context.Id) : 0;
-      const children = context.Children.map(getContext);
-
-      context.x = (parent.x || rootContextX) + (context.Depth * (500 / 4)) + ((childIndex * 2) * 500)
-      context.y = (parent.y || rootContextY) + (context.Depth * (500 / 4))
-      context.width = 500 + (children.length * 500)
-      context.height = 500 + (children.length * 500)
-
-      // If in the root context, increment the position for hte next root block.
-      if (!parent.x) {
-        rootContextX += context.width;
-        rootContextY += 0 //context.height;
-      }
-    });
-
-    let spacingByContext = {};
-    data.Gates.forEach(gate => {
-      // Calculate the width of this gate.
-      let gateWidth = 30;
-      if (gate.Type === 'BLOCK_INPUT' || gate.Type === 'BLOCK_OUTPUT') {
-        gateWidth = 20;
-      }
-      if (gate.Type === 'BUILTIN_FUNCTION' && gate.Label === 'tflipflop') {
-        gateWidth = 80;
-      }
-      gate.width = gateWidth;
-
-      const context = getContext(gate.CallingContext);
-      if (context) {
-        if (gate.Type === 'BLOCK_INPUT' || gate.Type === 'BLOCK_OUTPUT') {
-          // All inputs are positioned on the left border, and all outputs on the right
-          spacingByContext[context.Id] = spacingByContext[context.Id] || 0
-          gate.xPosition = context.x + (gate.Type === 'BLOCK_OUTPUT' ? context.width : 0)
-          gate.yPosition = context.y + spacingByContext[context.Id]
-          context.gateCount += 1
-
-          spacingByContext[context.Id] += gateWidth
-        } else {
-          // All the rest of the gates in a line below the inputs and outputs
-          spacingByContext[context.Id] = spacingByContext[context.Id] || 0
-          gate.xPosition = context.x + spacingByContext[context.Id]
-          gate.yPosition = context.y + 100
-          context.gateCount += 1
-          spacingByContext[context.Id] += gateWidth
-        }
-      } else {
-        // All the rest of the gates in a line below the inputs and outputs
-        spacingByContext[null] = spacingByContext[null] || 0
-        gate.xPosition = spacingByContext[null];
-        gate.yPosition = 0
-        spacingByContext[null] += gateWidth
-      }
-    });
-
-    // Move inputs and outputs closer to the gates that they conenct to
-    data.Gates.forEach(gate => {
-      if (!(gate.Type === 'BLOCK_INPUT' || gate.Type === 'BLOCK_OUTPUT')) {
-        return;
-      }
-
-      const context = getContext(gate.CallingContext);
-
-      const gatesInContext = data.Gates
-        .filter(i => i.Type === gate.Type && i.CallingContext.toString() === context.Id.toString());
-
-      const positionOfGateInBlock = gatesInContext.findIndex(i => i.Id === gate.Id);
-
-      if (gate.Type === 'BLOCK_OUTPUT') {
-        gate.xPosition -= 40;
-      }
-      gate.yPosition = context.y + positionOfGateInBlock * 30;
-    });
-
-    // Move gates closer to their inputs and outputs
-    data.Gates.forEach(gate => {
-      if (gate.Inputs.length === 0 || gate.Type === 'BLOCK_INPUT' || gate.Type === 'BLOCK_OUTPUT') {
-        return;
-      }
-
-      const gateConnectedToInput = data.Gates.find(g => {
-        return g.Outputs.map(k => k.Id).indexOf(gate.Inputs[0].Id) !== -1;
-      });
-
-      const wireLength = Math.sqrt(
-        Math.pow(gateConnectedToInput.xPosition - gate.xPosition, 2),
-        Math.pow(gateConnectedToInput.yPosition - gate.yPosition, 2),
-      );
-
-      if (wireLength > 100) {
-        gate.xPosition = gateConnectedToInput.xPosition + 50;
-        gate.yPosition = gateConnectedToInput.yPosition + 100;
-      }
-    });
-
-    // Final positioning step - make sure that gates don't intersect
-    data.Gates.forEach((gate, index) => {
-      if (gate.Type === 'BLOCK_INPUT' || gate.Type === 'BLOCK_OUTPUT') {
-        return;
-      }
-
-      data.Gates.slice(index + 1)
-        .filter(i => !(i.Type === 'BLOCK_INPUT' || i.Type === 'BLOCK_OUTPUT'))
-        .filter(i =>  // Find other gates that intersect with this gate.
-          i.xPosition >= gate.xPosition && i.xPosition <= gate.xPosition + 30 &&
-          i.yPosition >= gate.yPosition && i.yPosition <= gate.yPosition + 30
-        ).forEach((i, ct) => {
-          i.xPosition += ((ct + 1) * 40) 
-          // T flipflops are wider than normal gates, so add a bit of padding.
-          if (gate.Type === 'BUILTIN_FUNCTION' && gate.Label === 'tflipflop') {
-            i.xPosition += 60
-          }
-          i.yPosition += 10
-        });
-    });
-
-    // Rotate gates to try to ensure that they are optimally placed
-    data.Gates.forEach(gate => {
-      if (gate.Outputs.length === 0 || gate.Type === 'BUILTIN_FUNCTION' || gate.Type === 'BLOCK_INPUT' || gate.Type === 'BLOCK_OUTPUT') {
-        return;
-      }
-
-      const gateConnectedToInput = data.Gates.find(g => {
-        return g.Inputs.map(k => k.Id).indexOf(gate.Outputs[0].Id) !== -1;
-      });
-
-      if (gateConnectedToInput && gateConnectedToInput.yPosition > gate.yPosition + 50) {
-        gate.rotate = 180;
-      } else {
-        gate.rotate = 0;
-      }
-    });
-
+    data = positionGates(data)
 
     renderFrame(data.Gates.map(i => i.Id));
   }).catch(err => {
@@ -250,26 +289,21 @@ const compile = debounce(function compile(source) {
   });
 }, 1000);
 
-Object.defineProperty(window, 'gates', {
-  get: function() {
-    return data.Gates;
-  },
-  set: function(value) {
-    data.Gates = value;
-    renderFrame(data.Gates.map(i => i.Id));
-  },
-})
-
 
 
 let data = {Gates: [], Wires: [], Contexts: [], Outputs: []};
 let error = null;
-editor.on('change', () => {
-  const value = editor.getValue();
-  localStorage.source = value;
-  compile(value);
-});
-compile(editor.getValue());
+
+// When the user types in the editor, compile the source that they type and render it in the right
+// pane.
+if (editor) {
+  editor.on('change', () => {
+    const value = editor.getValue();
+    localStorage.source = value;
+    compile(value);
+  });
+  compile(editor.getValue());
+}
 
 // Get a reference to the svg viewport
 const viewport = document.getElementById('viewport');
@@ -294,7 +328,7 @@ function renderFrame(updatedGateIds) {
   if (gateState !== newGateState) {
     gateState = newGateState;
 
-    return window.fetch('http://localhost:8080/v1/run', {
+    return window.fetch(`${server}/v1/run`, {
       method: 'POST',
       body: JSON.stringify(data),
       headers: {
@@ -311,6 +345,10 @@ function renderFrame(updatedGateIds) {
       // Was an error received while compiling?
       if (updates.Error) {
         throw new Error(updates.Error);
+      }
+
+      if (updates.Gates.length === 0) {
+        return;
       }
 
       // Update the state of each gate, and the powered state of each wire
@@ -435,5 +473,16 @@ resizeBar.addEventListener('mousemove', function(event) {
   }
 });
 
-// Initial svg size
-resizePanes(resizePosition);
+// When the browser window is resized, adjust the sizing of all the components on-screen.
+function onViewportResize() {
+  if (!previewMode) {
+    // When in normal mode, start be resizing the panes.
+    resizePanes(resizePosition);
+  } else {
+    // In preview mode, make the svg full-width.
+    viewport.setAttribute('width', `${document.body.clientWidth}px`);
+    viewport.setAttribute('height', `${document.body.clientHeight}px`);
+  }
+}
+window.addEventListener('resize', onViewportResize);
+onViewportResize();
